e3bd8500a5f5ea02feb908d8153e71c049e024e0
lucas <lucas@MacBook-Air-de-lucas.local>
2024-09-17 15:24:39 +0200

    mise en prod du dashboard test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Filament\Facades\Filament;
 
 Route::get('/', function () {
     return view('LandingApi');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+// Ajoutez ces lignes pour Filament
+Route::domain(config('filament.domain'))
+    ->middleware(config('filament.middleware.base'))
+    ->prefix(config('filament.path'))
+    ->group(function () {
+        Filament::registerRenderHook(
+            'styles.after',
+            fn (): string => '<link rel="stylesheet" href="' . asset('css/custom.css') . '">'
+        );
+    });
