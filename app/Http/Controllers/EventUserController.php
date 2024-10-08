311a2c5224da4ddf2136dcaefbe1dafb8b64d50a
lucas <lucas@MacBook-Air-de-lucas.local>
2024-10-08 13:46:14 +0200

    push

--- app/Http/Controllers/EventUserController.php
+++ app/Http/Controllers/EventUserController.php
@@ -43,7 +43,7 @@ class EventUserController extends Controller
     public function destroy(int $userId)
     {
         $EventUser = EventUser::where('user_id', $userId && 'event_id')->first();
-        $EventUser = $this->eventUserRepository->delete($userId);
+        $EventUser = $this->eventUserRepository->delete($EventUser);
         return $this->jsonResponse('success', 'Eventuser deleted', ['users' => $EventUser], 204) ;
     }
 }
